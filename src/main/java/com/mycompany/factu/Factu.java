ffe42ac7b7d1214573321688f951a187409c18dd
erick <delabra32@gmail.com>
2023-01-09 13:08:52 -0600

    first

---
+++ src/main/java/com/mycompany/factu/Factu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.factu;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Factu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
